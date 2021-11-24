ab8b531408d7b1ce9e465313b85020f72dc9c1bc
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2021-11-25 01:09:25 +1100

    added debugging message

--- direct_mpich.cpp
+++ direct_mpich.cpp
@@ -37,7 +37,9 @@ int main( int argc, char** argv )
     // Get MPI rank and size
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
- 
+    if(rank==0)
+      printf("Running on %d MPI ranks.\n", size);
+    
     // Allocate host and device buffers and copy rank value to GPU
     bytes = size*sizeof(int);
     h_buff = (int*)malloc(bytes);
